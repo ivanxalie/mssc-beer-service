b55001773ef15eb024afaa059259bf23855f35f2
ivanxalie <ivanxalie@gmail.com>
2024-03-27 02:48:04 +0300

    Revert all changes to default

--- src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
+++ src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -1,7 +1,7 @@
 package guru.springframework.msscbeerservice;
 
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
